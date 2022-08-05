2409b846e874eff085188e2f181a5a1e9c720398
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2022-08-06 00:45:53 +0100

    Add placeholders for the anagram/wordfit parts of the API

--- crosswordtoolkit/crosswordtoolkitapi/urls.py
+++ crosswordtoolkit/crosswordtoolkitapi/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("anagram/<str:letters>", views.anagram, name="anagram"),
+    path("wordfit/<str:letters>", views.wordfit, name="wordfit"),
 ]
--- crosswordtoolkit/crosswordtoolkitapi/views.py
+++ crosswordtoolkit/crosswordtoolkitapi/views.py
@@ -1,8 +1,21 @@
+import json
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Crossword Toolkit API index.")
+
+
+def anagram(request, letters):
+    return HttpResponse(
+        f"Iya. You're now at the Crossword Toolkit Anagram API page, for input letters: {letters}!"
+    )
+
+
+def wordfit(request, letters):
+    """Placeholder function to find the words, with some test json output"""
+    words = []
+    for l in letters:
+        words.append(l)
+    return HttpResponse(f"{json.dumps(words)}")
