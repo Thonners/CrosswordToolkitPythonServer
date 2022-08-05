5b1cd0195969ad10d8f0b956b5bededc770def44
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2022-08-06 00:25:31 +0100

    Create initial DB schema

--- crosswordtoolkit/crosswordtoolkitapi/models.py
+++ crosswordtoolkit/crosswordtoolkitapi/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password_salted_hash = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Crossword(models.Model):
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publication_date = models.DateTimeField("Date published")
+    num_rows = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.publisher}: {self.publication_date}"
