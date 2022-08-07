17ab334b81fcc3cd885513dacd88e4aa13fdd026
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2022-08-07 14:33:30 +0100

    Return the actual results for the API search!

--- crosswordtoolkit/crosswordtoolkitapi/views.py
+++ crosswordtoolkit/crosswordtoolkitapi/views.py
@@ -1,6 +1,10 @@
 import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from crosswordtoolkitserver.server import CrosswordToolkitServer
+
+# Create the object instance now so that it's only loaded into memory once. It can then be used each time someone visits the page
+crossword_toolkit_server = CrosswordToolkitServer()
 
 
 def index(request):
@@ -8,14 +12,8 @@ def index(request):
 
 
 def anagram(request, letters):
-    return HttpResponse(
-        f"Iya. You're now at the Crossword Toolkit Anagram API page, for input letters: {letters}!"
-    )
+    return HttpResponse(json.dumps(crossword_toolkit_server.get_anagrams(letters)))
 
 
 def wordfit(request, letters):
-    """Placeholder function to find the words, with some test json output"""
-    words = []
-    for l in letters:
-        words.append(l)
-    return HttpResponse(f"{json.dumps(words)}")
+    return HttpResponse(json.dumps(crossword_toolkit_server.get_word_fit(letters)))
