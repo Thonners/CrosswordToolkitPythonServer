9c0a00fcd7dffa71a914dc1a4e4cc217a024010d
Mathonwy Thomas <mathonwy.thomas@arrival.com>
2022-08-16 16:34:12 +0100

    Refactor anagram methods into AnagramSolver class

--- crosswordtoolkitserver/anagram.py
+++ crosswordtoolkitserver/anagram.py
@@ -1,43 +1,48 @@
-def get_remaining_letters(letters: str, letters_to_be_removed: str) -> str:
-    other_letters = letters
-    for letter in letters_to_be_removed:
-        other_letters = other_letters.replace(letter, "", 1)
-    return other_letters
+class AnagramSolver:
+    def __init__(self) -> None:
+        pass
 
+    def get_remaining_letters(self, letters: str, letters_to_be_removed: str) -> str:
+        other_letters = letters
+        for letter in letters_to_be_removed:
+            other_letters = other_letters.replace(letter, "", 1)
+        return other_letters
 
-def get_letter_combos(letters: str, length_of_word: int) -> str:
-    for i in range(len(letters) - length_of_word + 1):
-        if length_of_word > 1:
-            # Until we're on the final letter, iterate recursively over the remaining letters and their combos
-            for remaining_letters in get_letter_combos(
-                letters[i + 1 :], length_of_word - 1
-            ):
-                # Yield this letter plus all the combos of the remaining letters
-                yield letters[i] + remaining_letters
-        else:
-            # If there's only 1 letter in the word, just return the letter!
-            yield letters[i]
+    def get_letter_combos(self, letters: str, length_of_word: int) -> str:
+        for i in range(len(letters) - length_of_word + 1):
+            if length_of_word > 1:
+                # Until we're on the final letter, iterate recursively over the remaining letters and their combos
+                for remaining_letters in self.get_letter_combos(
+                    letters[i + 1 :], length_of_word - 1
+                ):
+                    # Yield this letter plus all the combos of the remaining letters
+                    yield letters[i] + remaining_letters
+            else:
+                # If there's only 1 letter in the word, just return the letter!
+                yield letters[i]
 
+    def get_all_possible_words(self, letters: str, lengths: list[int]) -> list[str]:
+        if sum(lengths) != len(letters):
+            raise ValueError(
+                f"Mismatch between number of letters ({len(letters)}) and length of requested answers: {lengths}"
+            )
+        for first_word in self.get_letter_combos(letters, lengths[0]):
+            other_letters = self.get_remaining_letters(letters, first_word)
+            if len(lengths) == 2:
+                yield [first_word, other_letters]
+            elif len(lengths) > 2:
+                for other_words in self.get_all_possible_words(
+                    other_letters, lengths[1:]
+                ):
+                    yield [first_word, *other_words]
 
-def get_all_possible_words(letters: str, lengths: list[int]) -> list[str]:
-    if sum(lengths) != len(letters):
-        raise ValueError(
-            f"Mismatch between number of letters ({len(letters)}) and length of requested answers: {lengths}"
-        )
-    for first_word in get_letter_combos(letters, lengths[0]):
-        other_letters = get_remaining_letters(letters, first_word)
-        if len(lengths) == 2:
-            yield [first_word, other_letters]
-        elif len(lengths) > 2:
-            for other_words in get_all_possible_words(other_letters, lengths[1:]):
-                yield [first_word, *other_words]
-
-
-def get_possible_words_no_repeats(letters: str, lengths: list[int]) -> list[str]:
-    all_possible_words = get_all_possible_words(letters, lengths)
-    word_sets = []
-    for word_set in all_possible_words:
-        sorted_words = sorted(word_set)
-        if sorted_words not in word_sets:
-            word_sets.append(sorted_words)
-    return word_sets
+    def get_possible_words_no_repeats(
+        self, letters: str, lengths: list[int]
+    ) -> list[str]:
+        all_possible_words = self.get_all_possible_words(letters, lengths)
+        word_sets = []
+        for word_set in all_possible_words:
+            sorted_words = sorted(word_set)
+            if sorted_words not in word_sets:
+                word_sets.append(sorted_words)
+        return word_sets
--- crosswordtoolkitserver/tests/test_anagram.py
+++ crosswordtoolkitserver/tests/test_anagram.py
@@ -1,42 +1,45 @@
 from ..anagram import (
-    get_remaining_letters,
-    get_letter_combos,
-    get_all_possible_words,
-    get_possible_words_no_repeats,
+    AnagramSolver,
+    # get_remaining_letters,
+    # get_letter_combos,
+    # get_all_possible_words,
+    # get_possible_words_no_repeats,
 )
+
+anagram_solver = AnagramSolver()
 
 
 def test_remaining_letters():
     all_letters = "abc"
-    assert "bc" == get_remaining_letters(all_letters, "a")
+    assert "bc" == anagram_solver.get_remaining_letters(all_letters, "a")
     all_letters = "abcdef"
-    assert "def" == get_remaining_letters(all_letters, "abc")
-    assert "adef" == get_remaining_letters(all_letters, "bc")
-    assert "" == get_remaining_letters(all_letters, "abcdef")
-    assert "abc" == get_remaining_letters(all_letters, "def")
-    assert "ace" == get_remaining_letters(all_letters, "bdf")
+    assert "def" == anagram_solver.get_remaining_letters(all_letters, "abc")
+    assert "adef" == anagram_solver.get_remaining_letters(all_letters, "bc")
+    assert "" == anagram_solver.get_remaining_letters(all_letters, "abcdef")
+    assert "abc" == anagram_solver.get_remaining_letters(all_letters, "def")
+    assert "ace" == anagram_solver.get_remaining_letters(all_letters, "bdf")
 
 
 def test_remaining_letters_with_repeats():
     all_letters = "abcdefaaa"
-    assert "defaaa" == get_remaining_letters(all_letters, "abc")
-    assert "defaa" == get_remaining_letters(all_letters, "abca")
-    assert "defa" == get_remaining_letters(all_letters, "aaabc")
-    assert "defa" == get_remaining_letters(all_letters, "abcaa")
-    assert "abcfaaa" == get_remaining_letters(all_letters, "de")
+    assert "defaaa" == anagram_solver.get_remaining_letters(all_letters, "abc")
+    assert "defaa" == anagram_solver.get_remaining_letters(all_letters, "abca")
+    assert "defa" == anagram_solver.get_remaining_letters(all_letters, "aaabc")
+    assert "defa" == anagram_solver.get_remaining_letters(all_letters, "abcaa")
+    assert "abcfaaa" == anagram_solver.get_remaining_letters(all_letters, "de")
     all_letters = "aaabcdef"
-    assert "aadef" == get_remaining_letters(all_letters, "abc")
-    assert "adef" == get_remaining_letters(all_letters, "abca")
-    assert "def" == get_remaining_letters(all_letters, "aaabc")
-    assert "def" == get_remaining_letters(all_letters, "abcaa")
-    assert "aaabcf" == get_remaining_letters(all_letters, "de")
+    assert "aadef" == anagram_solver.get_remaining_letters(all_letters, "abc")
+    assert "adef" == anagram_solver.get_remaining_letters(all_letters, "abca")
+    assert "def" == anagram_solver.get_remaining_letters(all_letters, "aaabc")
+    assert "def" == anagram_solver.get_remaining_letters(all_letters, "abcaa")
+    assert "aaabcf" == anagram_solver.get_remaining_letters(all_letters, "de")
 
 
 def test_get_letter_combos_short():
     all_letters = "abc"
-    assert ["a", "b", "c"] == list(get_letter_combos(all_letters, 1))
-    assert ["ab", "ac", "bc"] == list(get_letter_combos(all_letters, 2))
-    assert ["abc"] == list(get_letter_combos(all_letters, 3))
+    assert ["a", "b", "c"] == list(anagram_solver.get_letter_combos(all_letters, 1))
+    assert ["ab", "ac", "bc"] == list(anagram_solver.get_letter_combos(all_letters, 2))
+    assert ["abc"] == list(anagram_solver.get_letter_combos(all_letters, 3))
 
 
 def test_get_letter_combos_longer():
@@ -52,7 +55,7 @@ def test_get_letter_combos_longer():
         "bce",
         "bde",
         "cde",
-    ] == list(get_letter_combos(all_letters, 3))
+    ] == list(anagram_solver.get_letter_combos(all_letters, 3))
 
 
 def test_get_all_two_words():
@@ -62,7 +65,7 @@ def test_get_all_two_words():
         ["ab", "c"],
         ["ac", "b"],
         ["bc", "a"],
-    ] == list(get_all_possible_words(all_letters, lengths))
+    ] == list(anagram_solver.get_all_possible_words(all_letters, lengths))
     # Longer...
     all_letters = "abcdef"
     lengths = [5, 1]
@@ -73,7 +76,7 @@ def test_get_all_two_words():
         ["abdef", "c"],
         ["acdef", "b"],
         ["bcdef", "a"],
-    ] == list(get_all_possible_words(all_letters, lengths))
+    ] == list(anagram_solver.get_all_possible_words(all_letters, lengths))
 
 
 def test_get_all_three_words():
@@ -86,13 +89,15 @@ def test_get_all_three_words():
         ["b", "c", "a"],
         ["c", "a", "b"],
         ["c", "b", "a"],
-    ] == list(get_all_possible_words(all_letters, lengths))
+    ] == list(anagram_solver.get_all_possible_words(all_letters, lengths))
 
 
 def test_get_words_no_repeats():
     letters = "abc"
     lengths = [1, 1, 1]
-    assert [["a", "b", "c"]] == get_possible_words_no_repeats(letters, lengths)
+    assert [["a", "b", "c"]] == anagram_solver.get_possible_words_no_repeats(
+        letters, lengths
+    )
     letters = "abcd"
     lengths = [1, 1, 2]
     assert [
@@ -102,4 +107,4 @@ def test_get_words_no_repeats():
         ["ad", "b", "c"],
         ["ac", "b", "d"],
         ["ab", "c", "d"],
-    ] == get_possible_words_no_repeats(letters, lengths)
+    ] == anagram_solver.get_possible_words_no_repeats(letters, lengths)
