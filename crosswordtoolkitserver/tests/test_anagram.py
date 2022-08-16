380120c51aeb0b0767c6197523c2ed610281a430
Mathonwy Thomas <mathonwy.thomas@arrival.com>
2022-08-16 14:35:04 +0100

    Add tests or get_letter_combos

--- crosswordtoolkitserver/tests/test_anagram.py
+++ crosswordtoolkitserver/tests/test_anagram.py
@@ -17,3 +17,26 @@ def test_remaining_letters_with_repeats():
     assert "defa" == get_remaining_letters(all_letters, "aaabc")
     assert "defa" == get_remaining_letters(all_letters, "abcaa")
     assert "abcfaaa" == get_remaining_letters(all_letters, "de")
+
+
+def test_get_letter_combos_short():
+    all_letters = "abc"
+    assert ["a", "b", "c"] == list(get_letter_combos(all_letters, 1))
+    assert ["ab", "ac", "bc"] == list(get_letter_combos(all_letters, 2))
+    assert ["abc"] == list(get_letter_combos(all_letters, 3))
+
+
+def test_get_letter_combos_longer():
+    all_letters = "abcde"
+    assert [
+        "abc",
+        "abd",
+        "abe",
+        "acd",
+        "ace",
+        "ade",
+        "bcd",
+        "bce",
+        "bde",
+        "cde",
+    ] == list(get_letter_combos(all_letters, 3))
