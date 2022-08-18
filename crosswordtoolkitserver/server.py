858fecb81b80821897caf1830a46f4d3e91bee50
Mathonwy Thomas <mathonwy.thomas@arrival.com>
2022-08-18 09:39:23 +0100

    Simplify the CrosswordToolkitServer class to wrap the AnagramSolver and Dictionary classes

--- crosswordtoolkitserver/server.py
+++ crosswordtoolkitserver/server.py
@@ -1,44 +1,14 @@
-import os
-import json
-from pathlib import Path
-import re
+from .dictionary import CrosswordToolkitDictionary
+from .anagram import AnagramSolver
 
 
 class CrosswordToolkitServer:
     def __init__(self):
-        self.load_words()
+        self.dictionary = CrosswordToolkitDictionary()
+        self.anagram_solver = AnagramSolver(self.dictionary)
 
-    def _sort_letters(self, letters):
-        return "".join((sorted(letters)))
-
-    def load_words(self):
-        self.words = {}
-        self.anagram = {}
-        for i in range(2, 16):
-            this_script_path = Path(__file__)
-            filepath = Path(
-                this_script_path.parent, "static", "words", f"{i}-letter-words.json"
-            )
-            with open(filepath, "r") as file:
-                json_words = json.load(file)
-                self.words[i] = [word["word"] for word in json_words]
-                self.anagram[i] = {}
-                for word in self.words[i]:
-                    sorted_letters = self._sort_letters(word)
-                    if sorted_letters in self.anagram[i]:
-                        self.anagram[i][sorted_letters].append(word)
-                    else:
-                        self.anagram[i][sorted_letters] = [word]
-
-    def get_anagrams(self, letters):
-        try:
-            return self.anagram[len(letters)][self._sort_letters(letters)]
-        except KeyError:
-            return []
+    def get_anagrams(self, letters, lengths=None):
+        return self.anagram_solver.get_anagrams(letters, lengths)
 
     def get_word_fit(self, letters):
-        r = re.compile(letters)
-        try:
-            return list(filter(r.match, self.words[len(letters)]))
-        except KeyError:
-            return []
+        return self.dictionary.get_word_fit(letters)
