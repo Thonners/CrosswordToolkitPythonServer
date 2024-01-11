c67e1d9ddff513288f741173c3dc61dbaf8d0fba
Crossword Toolkit Server <crosswordtoolkit@localhost.localdomain>
2024-01-11 22:44:05 +0000

    Add multi-word anagram support for up to 2 words

--- crosswordtoolkitserver/dictionary.py
+++ crosswordtoolkitserver/dictionary.py
@@ -1,7 +1,7 @@
-import os
 import json
 from pathlib import Path
 import re
+from itertools import combinations
 
 
 class CrosswordToolkitDictionary:
@@ -32,7 +32,53 @@ class CrosswordToolkitDictionary:
                     else:
                         self.anagram[i][sorted_letters] = [word]
 
-    def get_anagrams(self, letters):
+
+    def get_multiword_anagrams(self, letters:str, word_lengths: list[int] | None = None):
+        num_letters = len(letters)
+        if word_lengths is None or len(word_lengths) == 0:
+            word_lengths = [num_letters]
+        else:
+            if total_word_letters := sum(word_lengths) != num_letters:
+                raise ValueError(f"Total length of words ({total_word_letters}) not equal to the number of available letters ({num_letters})")
+            word_lengths = sorted(word_lengths)
+        sorted_letters = "".join(sorted(letters))
+        combos = self.get_letter_combos(sorted_letters, word_lengths)
+        anagrams = []
+        for combo in combos:
+            is_anagram = True
+            word_anagrams = []
+            for potential_word in combo:
+                word_length = len(potential_word)
+                if potential_word in self.anagram[word_length]:
+                    word_anagrams.append(self.anagram[word_length][potential_word])
+                else:
+                    is_anagram = False
+            if is_anagram:
+                anagrams.append(word_anagrams)
+        return anagrams
+
+
+
+    def get_letter_combos(self, letters :str, word_lengths):
+        if len(word_lengths) == 0:
+            raise ValueError("Word lengths must contain at least one item. Got []")
+        if len(word_lengths) == 1:
+            if word_lengths[0] != len(letters):
+                raise ValueError(f"Total length of words ({word_lengths[0]}) not equal to the number of available letters ({len(letters)})")
+            return letters
+        
+        word_letter_combos = combinations(letters, word_lengths[0])
+        output  = []
+        for first_word_letters in word_letter_combos:
+            first_word = "".join(first_word_letters)
+            remaining_letters = letters
+            for letter in first_word_letters:
+                remaining_letters = remaining_letters.replace(letter,"",1)
+            remaining_words = word_lengths[1:]
+            output.append((first_word, self.get_letter_combos(remaining_letters, remaining_words)))
+        return set(output)
+    
+    def get_anagrams(self, letters:str):
         try:
             return self.anagram[len(letters)][self.sort_letters(letters)]
         except KeyError:
